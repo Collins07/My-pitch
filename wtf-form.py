b0da17aa6f604b0c8d52c3ec44a7dff02eb887e2
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-05-09 06:43:02 +0300

    password field validator

--- wtf-form.py
+++ wtf-form.py
@@ -1,8 +1,8 @@
 import email
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Length,Email
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=10)])
-    email = StringField('Email', validators=[DataRequired(), Email() )
+    email = StringField('Email', validators=[DataRequired(), Email()])
