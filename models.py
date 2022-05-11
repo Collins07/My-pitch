8ae2aa53bab5628d2c15637425509bff8474141b
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-05-11 20:38:19 +0300

    Post model

--- models.py
+++ models.py
@@ -1,6 +1,19 @@
+from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 from app import db
 
 
 class User(db.model):
-    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(120),nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False)
