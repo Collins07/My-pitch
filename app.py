fdb7ee43d1a12ca3e1a0bf5a04b1708455613e39
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-05-12 23:37:52 +0300

    password hashing

--- app.py
+++ app.py
@@ -56,6 +56,8 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account created for { form.username.data }', 'success')
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         return redirect(url_for('register'))
     return render_template('register.html', form=form)
 
