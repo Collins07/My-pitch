72592b2957e593a3964fa98bc72752f9a8632565
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-05-13 00:18:58 +0300

    user validation

--- form.py
+++ form.py
@@ -1,7 +1,9 @@
 import email
+from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, Length,Email
+from wtforms.validators import DataRequired, EqualTo, Length,Email,ValidationError
+from app import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=10)])
@@ -10,6 +12,12 @@ class RegistrationForm(FlaskForm):
     repeat_password = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
 
     submit = SubmitField('Sign-Up')
+
+    def validate_field(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationErr('The username is taken')
+
 
 
 class LoginForm(FlaskForm):
